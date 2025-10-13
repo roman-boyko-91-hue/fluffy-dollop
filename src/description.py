1179882853fd67e8a59e55cfb6892621b4bc4728
Roman <roman-boyko-91@mail.ru>
2025-10-13 21:25:49 +0500

    Commit new func

--- src/description.py
+++ src/description.py
@@ -1,7 +1,9 @@
 import re
-import openpyxl
+import pandas as pd
 
 file_path = r"C:\Users\1\PycharmProjects\pythonProject\transactions_excel.xlsx"
+transactions_df = pd.read_excel(file_path)
+transactions_list = transactions_df.to_dict(orient='records')
 
 
 def search_transactions(transactions: list[dict], search: str) -> list[dict]:
@@ -11,10 +13,12 @@ def search_transactions(transactions: list[dict], search: str) -> list[dict]:
     """
     results = []
     for transaction in transactions:
-        if re.search(search, transaction['description'], flags=re.IGNORECASE) and isinstance(transaction['description'], str):
-            if search in transaction['description']:
+        if isinstance(transaction['description'], (str, float)):
+            description = str(transaction['description'])
+            if re.search(search, description, flags=re.IGNORECASE):
                 results.append(transaction)
+
     return results
 
 
-print(search_transactions(transactions, search))
+print(search_transactions(transactions_list, 'перевод'))
