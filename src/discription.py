83339a4014d5975fda812020fc6c23764622cb78
Roman <roman-boyko-91@mail.ru>
2025-10-13 20:13:44 +0500

    Commit new branch

--- src/discription.py
+++ src/discription.py
@@ -1,16 +1,19 @@
 import re
 
+file_path = r"C:\Users\1\PycharmProjects\pythonProject\transactions_excel.xlsx"
+
 
 def search_transactions(transactions: list[dict], search: str) -> list[dict]:
     """
     Функция принимает список словарей с данными о банковских операциях и
     строку поиска, а возвращает список словарей, в описании кт есть данная строка.
     """
-    results = re.findall(r"[dict]")
+    results = []
     for transaction in transactions:
-        if 'description' in transaction and isinstance(transaction['description'], str):
+        if re.search(search, description, flags=re.IGNORECASE) and isinstance(transaction['description'], str):
             if search in transaction['description']:
                 results.append(transaction)
     return results
+
 
 print(search_transactions(transactions, search))
