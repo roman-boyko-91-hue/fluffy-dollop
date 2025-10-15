969696b509c7a91d9b95c502937d2ccdf30866e9
Roman <roman-boyko-91@mail.ru>
2025-10-15 20:01:38 +0500

    New commit tests

---
+++ tests/test_new_func.py
@@ -0,0 +1,21 @@
+import pytest
+from src.description import search_transactions
+from src.description_list import process_bank_operations
+
+
+def test_search_transactions(transactions):
+    search = "Маркет"
+    result = []
+    assert search_transactions(transactions, search) == result
+
+
+def test_process_bank_operations():
+    transactions = [
+        {'description': 'Перевод', 'amount': 100},
+        {'description': 'Оплата', 'amount': 200},
+    ]
+    categories = ['Перевод', 'Оплата']
+    expected_result = {'Перевод': 1, 'Оплата': 1}
+
+    result = process_bank_operations(transactions, categories)
+    assert result == expected_result
