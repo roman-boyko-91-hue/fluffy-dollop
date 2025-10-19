996583dc17a2d831ec43945ecab65e224a1d8b41
Roman <roman-boyko-91@mail.ru>
2025-10-19 10:37:36 +0500

    New commit

--- tests/test_new_func.py
+++ tests/test_new_func.py
@@ -1,6 +1,5 @@
 from unittest.mock import mock_open, patch
 
-from main.main import main
 from src.description import search_transactions
 from src.description_list import process_bank_operations
 
@@ -23,15 +22,3 @@ def test_process_bank_operations():
 
     result = process_bank_operations(transactions, categories)
     assert result == expected_result
-
-
-def test_main():
-    """Тестирование функции, в которой подставляются данные вместо реального содержимого файла"""
-    mock_data = '[{"id": 4419458862}]'
-
-    # Используем patch для замены функции open на mock_open
-    with patch('builtins.open', mock_open(read_data=mock_data)):
-        transactions = main('path\to\mock_file.json')
-
-        # Проверяем, что данные из mock_data были считаны и обработаны правильно
-        assert transactions == [{"id": 4419458862}]
