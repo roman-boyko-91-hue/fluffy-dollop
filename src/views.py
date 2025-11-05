c1407818b51a83713cae847c7707f85d0afd9215
Roman <roman-boyko-91@mail.ru>
2025-11-05 20:47:01 +0500

    New commit file

--- src/views.py
+++ src/views.py
@@ -4,14 +4,15 @@ from utils import get_greetings
 
 
 def main(date: str) -> dict:
-    """Главная функция, принимающую на вход строку с датой и временем в формате
+    """Главная функция, принимающая на вход строку с датой и временем в формате
 YYYY-MM-DD HH:MM:SS и возвращающая JSON-ответ с некими данными"""
     date = datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
     result = {}
     greetings = get_greetings(date)
     result["greetings"] = greetings
-    card_info =
-    pass
+    card_info = list(r"../data/operations.xlsx")
+    card_info.append(card_info)
+
     return result
 
 print(main("2025-11-05 00:00:00"))
