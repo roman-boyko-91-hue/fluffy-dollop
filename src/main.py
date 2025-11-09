49cb8cd42972dedb0e9e4740c17620e2a2e61f96
Roman <roman-boyko-91@mail.ru>
2025-11-09 14:44:14 +0500

    New commit files

--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
+import pandas as pd
+
 from views import main
 from services import cashback_analyze
+from reports import spending_by_category
 
 if __name__ == "__main__":
     date = "2020-04-11 12:00:00"
     result_view = main(date)
-    #print(result_view)
+    print(result_view)
     result_sevices = cashback_analyze("../data/operations.xlsx", 2018, 3)
     print(result_sevices)
+
+    df = pd.read_excel("../data/operations.xlsx", sheet_name="Отчет по операциям")
+    result_reports = spending_by_category(df, "Ж/д билеты", "2020-04-11")
---
+++ src/reports.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+
+def spending_by_category(transactions: pd.DataFrame, category: str, data: str) -> dict:
+    """Функция, которая возвращает траты по категориям"""
+    print(transactions)
