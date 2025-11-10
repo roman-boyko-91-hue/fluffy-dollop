9aab203b6b1ba28a236fffd01f80688ad30892b7
Roman <roman-boyko-91@mail.ru>
2025-11-10 20:19:00 +0500

    New commit files

--- src/main.py
+++ src/main.py
@@ -7,9 +7,10 @@ from reports import spending_by_category
 if __name__ == "__main__":
     date = "2020-04-11 12:00:00"
     result_view = main(date)
-    #print(result_view)
+    # print(result_view)
     result_sevices = cashback_analyze("../data/operations.xlsx", 2018, 3)
-    print(result_sevices)
 
     df = pd.read_excel("../data/operations.xlsx", sheet_name="Отчет по операциям")
+    transactions = df.to_dict(orient='records')
     result_reports = spending_by_category(df, "Ж/д билеты", "2020-04-11")
+    print(result_sevices)
--- src/services.py
+++ src/services.py
@@ -1,19 +1,16 @@
 import json
 
 import pandas as pd
+from typing import List, Dict
 
 
-def cashback_analyze(file_path: str, year: int, month: int) -> dict:
+def cashback_analyze(transactions: List[Dict], year: int, month: int) -> dict:
     """Функция, которая анализирует выгодные категории кэшбэка"""
-    df = pd.read_excel(file_path)
+    df = pd.DataFrame(transactions)
     df["Дата операции"] = pd.to_datetime(df["Дата операции"], format="%d.%m.%Y %H:%M:%S")
-    filter_data = df[
-        (df["Дата операции"].dt.year == year)
-        &
-        (df["Дата операции"].dt.month == month)
-        &
-        (df["Кэшбэк"] > 0)
-        ]
+    filter_data = df[(df["Дата операции"].dt.year == year) &
+                     (df["Дата операции"].dt.month == month) &
+                     (df["Кэшбэк"] > 0)]
     expenses_by_category = filter_data.groupby("Категория")["Сумма платежа"].sum()
     cashback_by_category = abs(expenses_by_category) // 100
 
