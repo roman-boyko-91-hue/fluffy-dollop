5cebe4c4125180ca4ecb8e011951fae496163b5b
Roman <roman-boyko-91@mail.ru>
2025-11-22 18:06:07 +0500

    Commit for moduls

--- tests/test_category.py
+++ tests/test_category.py
@@ -7,4 +7,13 @@ def test_category(category):
             "description": "256GB, Серый цвет, 200MP камера",
             "price": 180000.0,
             "quantity": 5
-        }]
+        },
+        {
+            "name": "Iphone 15",
+            "description": "512GB, Gray space",
+            "price": 210000.0,
+            "quantity": 8
+        }
+    ]
+    assert category.category_count == 1
+    assert category.product_count == 2
