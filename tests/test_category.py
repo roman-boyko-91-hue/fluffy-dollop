23c517af1a1b2378aa231ae226c5b927b78cb74b
Roman <roman-boyko-91@mail.ru>
2025-12-06 15:24:15 +0500

    New commit

--- tests/test_category.py
+++ tests/test_category.py
@@ -4,3 +4,4 @@ import pytest
 def test_category_starts_with_no_product(empty_category):
     assert empty_category.name == "Default Category"
     assert empty_category.product == "В этой категории нет товаров"
+    assert empty_category.description == "Description"
