8dd20146e32fb15cc8595a43b6f2e3f442c7e011
Roman <roman-boyko-91@mail.ru>
2025-12-20 18:54:23 +0500

    New commit

--- tests/test_mixin_print.py
+++ tests/test_mixin_print.py
@@ -1,4 +1,3 @@
-import pytest
 from main.product import Product
 
 
--- tests/test_product.py
+++ tests/test_product.py
@@ -6,6 +6,7 @@ def test_product_attribute_change():
     product.price = 140000.0
     assert product.price == 140000.0
 
+
 def test_product_attribute_modification():
     product = Product("Samsung", "64GB", 50000.0, 3)
     product.price = 55000.0
