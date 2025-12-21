924e0b7d04bc23c928cef39706af2726bebd3a80
Roman <roman-boyko-91@mail.ru>
2025-12-21 16:57:36 +0500

    New commit

--- main/mixin_print.py
+++ main/mixin_print.py
@@ -1,7 +1,8 @@
 class MixinPrint:
     """Класс-миксин для печати в консоль информации в читаемом виде"""
+
     def __init__(self):
         print(repr(self))
 
     def __repr__(self):
-        return f"{self.__class__.__name__}{self.name}, {self.description}, {self.price}, {self.quantity}"
+        return f"Product('{self.name}', '{self.description}', {self.price}, {self.quantity})"
--- tests/test_mixin_print.py
+++ tests/test_mixin_print.py
@@ -11,3 +11,9 @@ def test_product_repr():
     product = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
     expected_repr = "Product(Samsung Galaxy S23 Ultra, 256GB, Серый цвет, 200MP камера, 180000.0, 5)"
     assert repr(product) == expected_repr
+
+
+def test_repr_and_recreation(product_5):
+    # Проверяем, что repr возвращает ожидаемую строку
+    expected_repr = "Product(Xiaomi Redmi Note 11, 1024GB, Синий, 31000.0, 14)"
+    assert repr(product_5) == expected_repr
