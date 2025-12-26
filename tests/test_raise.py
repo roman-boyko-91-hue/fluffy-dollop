460ffcc752df153549ead0142bbd834e96fae208
Roman <roman-boyko-91@mail.ru>
2025-12-26 05:38:00 +0500

    New commit

--- tests/test_raise.py
+++ tests/test_raise.py
@@ -4,12 +4,12 @@ from main.category import Category
 from main.product import Product
 
 
-def test_middle_category_exception_handling():
+def test_middle_category_exception():
     category = Category("Смартфоны", "Категория смартфонов", [])
     try:
         assert category.middle_category() == 0
     except Exception as e:
-        assert False, f"Exception was raised: {e}"
+        assert False, f"Исключение: {e}"
 
 
 def test_product_not_quantity_raises_error():
@@ -17,7 +17,7 @@ def test_product_not_quantity_raises_error():
         product = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 0)
 
 
-def test_middle_category_no_products():
+def test_middle_category_products():
     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 1)
     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
     category = Category("Смартфоны", "Категория смартфонов", [product1, product2])
@@ -27,3 +27,6 @@ def test_middle_category_no_products():
 def test_negative_price_raises_value_error():
     with pytest.raises(ValueError, match="Цена не может быть отрицательной!"):
         product = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", - 180000.0, 1)
+
+def test_other_product():
+    pass
