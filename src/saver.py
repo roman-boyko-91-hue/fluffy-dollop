e979362ac082f64dab1bf3f8a60029ddb1cf8c0b
Roman <roman-boyko-91@mail.ru>
2026-02-07 09:58:08 +0500

    New commit

--- src/saver.py
+++ src/saver.py
@@ -1,13 +1,38 @@
-class JSONSaver:
-    def add_vacancy(self, vacancy):
-        # Логика для добавления вакансии в JSON-файл
-        pass
+import json
+import os
 
-    def delete_vacancy(self, vacancy):
-        # Логика для удаления вакансии из JSON-файла
-        pass
+
+class JSONSaver:
+    def add_vacancy(self, new_vacancy):
+        file_path = 'data/vacancies.json'
+        if os.path.exists(file_path):
+            with open(file_path, 'r', encoding='utf-8') as file:
+                try:
+                    data = json.load(file)
+                except json.JSONDecodeError:
+                    data = []
+        else:
+            data = []
+
+        data.append(new_vacancy)
+
+        with open(file_path, 'w', encoding='utf-8') as file:
+            json.dump(data, file, indent=4, ensure_ascii=False)
+
+    def delete_vacancy(self, vacancy_id):
+        file_path = 'data/vacancies.json'
+        if os.path.exists(file_path):
+            with open(file_path, 'r', encoding='utf-8') as f:
+                data = json.load(f)
+        else:
+            data = []
+
+        updated_data = [v for v in data if v['id'] != vacancy_id]
+
+        with open(file_path, 'w', encoding='utf-8') as f:
+            json.dump(updated_data, f, indent=4, ensure_ascii=False)
 
 
 json_saver = JSONSaver()
-json_saver.add_vacancy()
+json_saver.add_vacancy({'id': 1, 'title': 'Python Developer'})
 json_saver.delete_vacancy()
