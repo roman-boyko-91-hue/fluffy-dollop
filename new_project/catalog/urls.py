51211fb023d23f0a203d2669c375abeb10026f3b
Roman <roman-boyko-91@mail.ru>
2026-03-18 19:22:21 +0500

    Initial commit

--- new_project/catalog/urls.py
+++ new_project/catalog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from new_project.catalog.apps import CatalogConfig
+from new_project.catalog.views import home
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('',)
+    path('', home, name='home')
 ]
