6aa60e56bbc0ab1d3386abb24005c749442353f5
Roman <roman-boyko-91@mail.ru>
2026-03-19 22:01:54 +0500

    New commit

---
+++ DjangoProject/catalog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from apps import CatalogConfig
+from views import home, contacts
+
+app_name = CatalogConfig.name
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('', contacts, name='contacts')
+]
