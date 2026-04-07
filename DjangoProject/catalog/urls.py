9f45d31d7e1ee45d5fb8edcfa971dc0202df8554
Roman <roman-boyko-91@mail.ru>
2026-04-07 19:35:22 +0500

    New commits 24

--- DjangoProject/catalog/urls.py
+++ DjangoProject/catalog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from .apps import CatalogConfig
-from .views import home, contacts
+from .views import home, contacts, product_detail
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', home, name='home'),
-    path('contacts/', contacts, name='contacts')
+    path('contacts/', contacts, name='contacts'),
+    path('product/<int:pk>/', product_detail, name='product_detail'),
 ]
--- DjangoProject/catalog/views.py
+++ DjangoProject/catalog/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
+
 
 def home(request):
-    return render(request, 'home.html')
+    product_list = Product.objects.all()
+    context = {
+        'object_list': product_list,
+        'title': 'Наш каталог'
+    }
+    return render(request, 'catalog/home.html', context)
+
 
 def contacts(request):
     return render(request, 'contacts.html')
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'catalog/product_detail.html', {'product': product})
